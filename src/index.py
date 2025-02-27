b490f430dbc692c7d71bf9247b816349926ab6a7
sai2698 <naveendevarapalli99@gmail.com>
2025-02-27 21:14:06 +0530

    Update index.py

--- src/index.py
+++ src/index.py
@@ -43,18 +43,32 @@ async def hello_message(dto: ISayHelloDto):
     return {"message": f"Hello {dto.message}"}
 
 class Graph:
-    settings: SectionProxy
+    settings: SimpleNamespace
     device_code_credential: DeviceCodeCredential
     user_client: GraphServiceClient
 
-    def __init__(self, config: SectionProxy):
+    def __init__(self, config: SimpleNamespace):
         self.settings = config
-        client_id = self.settings['clientId']
-        tenant_id = self.settings['tenantId']
-        graph_scopes = self.settings['graphUserScopes'].split(' ')
+        client_id = self.settings.clientId
+        tenant_id = self.settings.tenantId
+        graph_scopes = self.settings.graphUserScopes.split(' ')
 
         self.device_code_credential = DeviceCodeCredential(client_id, tenant_id=tenant_id)
         self.user_client = GraphServiceClient(self.device_code_credential, graph_scopes)
+
+# class Graph:
+#     settings: SectionProxy
+#     device_code_credential: DeviceCodeCredential
+#     user_client: GraphServiceClient
+
+#     def __init__(self, config: SectionProxy):
+#         self.settings = config
+#         client_id = self.settings['clientId']
+#         tenant_id = self.settings['tenantId']
+#         graph_scopes = self.settings['graphUserScopes'].split(' ')
+
+#         self.device_code_credential = DeviceCodeCredential(client_id, tenant_id=tenant_id)
+#         self.user_client = GraphServiceClient(self.device_code_credential, graph_scopes)
 
     async def get_user_token(self):
         graph_scopes = self.settings['graphUserScopes']
